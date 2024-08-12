d464a7f644194ebd9a99d485ca59acbed3b5bbf5
Ben Kelly <benjamink8@gmail.com>
2024-08-12 12:50:43 -0700

    new revisions - fixing y-coordinates for cars to drive on correct side of the road

--- ProjectSummer24_Frames.py
+++ ProjectSummer24_Frames.py
@@ -14,13 +14,24 @@ print(road_seg)
 for i in vehicle_series:
     print(i)
 
-
 def img_background(road_seg_list, loop_counter):
     image = Image.new("RGB", (1000, 1000), "black")
     mydraw = ImageDraw.Draw(image)
     mydraw.text((200, 5), "Traffic Simulator: Frame #" + str(loop_counter), align="center", fill="white", font_size=24)
     for seg in road_seg_list:
-        mydraw.line([(seg["x1a"], seg["y1a"]), (seg["x2a"], seg["y2a"])], width=10, fill="green", joint="curve")
+        if (seg["x1b"] < seg["x2a"]):
+            x1 = seg["x1b"]
+            x2 = seg["x2a"]
+        else:
+            x1 = seg["x2a"]
+            x2 = seg["x1b"]
+        if (seg["y1b"] > seg["y2a"]):
+            y1 = seg["y1b"]
+            y2 = seg["y2a"]
+        else:
+            y1 = seg["y2a"]
+            y2 = seg["y1b"]
+        mydraw.rectangle([(x1, 1000 - y1), (x2, 1000 - y2)], fill="green", outline="green", width=10)
     return image
 
 frames = []
@@ -31,7 +42,7 @@ for i in vehicle_series:
     frame_cur = img_background(road_seg, loop_count)
     draw = ImageDraw.Draw(frame_cur)
     for veh in i:
-        draw.ellipse((veh["x"]-2, veh["y"]-2, veh["x"]+2, veh["y"]+2), outline="white", width=3, fill="yellow")
+        draw.ellipse((veh["x"]-2, 1000 - (veh["y"]+2), veh["x"]+2, 1000 - (veh["y"]-2)), outline="white", width=3, fill="yellow")
     frames.append(frame_cur)
     loop_count += 1
 
